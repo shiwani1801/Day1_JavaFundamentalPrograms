cef5cc6e4210b5bebc3be40d4d32c0d4fe474f9a
shiwani <shiwanipaunikar1@gmail.com>
2023-01-07 13:30:58 -0800

    Added:Java fundamental programs

---
+++ src/com/bridgelabz/javafundamental/NameUsingCommandLineArg.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.javafundamental;
+
+public class NameUsingCommandLineArg {
+    public static void main(String[] args) {
+        String arg1 = args[0];
+        System.out.println(arg1);
+    }
+}
---
+++ src/com/bridgelabz/javafundamental/TwoStringsAreEqual.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.javafundamental;
+
+public class TwoStringsAreEqual {
+    public static void main(String[] args) {
+        String a = "shiwani";
+        String b = "shivani";
+
+        System.out.println( a.equals(b));
+    }
+
+}
